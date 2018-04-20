c11cf6e1beee3c0ab882e507b7207d0e90d5bdf5
Cerv1 <cervick13@gmail.com>
2018-04-20 12:41:06 +0200

    Descartando nuxeo, incompatible

--- src/index.js
+++ src/index.js
@@ -3,27 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-var Nuxeo = require('nuxeo');
 
-// create the Nuxeo client
-var nuxeo = new Nuxeo({
-    auth: {
-      method: 'basic',
-      username: 'Administrator',
-      password: 'Administrator'
-    }
-});
-
-nuxeo.connect()
-  .then(function(client){
-    // client.connected === true
-    // client === nuxeo
-    console.log('Connected OK!');
-  })
-  .catch(function(error) {
-    // wrong credentials / auth method / ...
-    throw error;
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
