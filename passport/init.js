89b12334b6a55e655a964ca07c8f6661fcad3563
Cerv1 <cervick13@gmail.com>
2018-04-25 10:35:40 +0200

    Añadiendo estilo para las funcionalidades del usuario una vez identificado

--- passport/init.js
+++ passport/init.js
@@ -2,17 +2,17 @@ var login = require('./login');
 var signup = require('./signup');
 var User = require('../models/user');
 
-module.exports = function(passport){
+module.exports = function (passport) {
 
-	// Passport needs to be able to serialize and deserialize users to support persistent login sessions
-    passport.serializeUser(function(user, done) {
-        console.log('serializing user: ');console.log(user);
+    // Passport needs to be able to serialize and deserialize users to support persistent login sessions
+    passport.serializeUser(function (user, done) {
+        console.log('serializing user: '); console.log(user);
         done(null, user._id);
     });
 
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            console.log('deserializing user:',user);
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            console.log('deserializing user:', user);
             done(err, user);
         });
     });
