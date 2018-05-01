a33b95d77f97bdf895eda175c06aaa5a20f30b8c
Cerv1 <cervick13@gmail.com>
2018-05-01 11:31:16 +0200

    Actualizando el modelo de incidencia para contemplar hora y fecha por separados

--- models/incident.js
+++ models/incident.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Incident', {
    number_id: Number,
    date: String,
+   time: String,
    dep: String,
    cen: Number,
    ppll: Number,
