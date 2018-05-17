a828f835fd85c0b9971add6c251b85300d90d220
Cerv1 <cervick13@gmail.com>
2018-05-17 10:58:10 +0200

    El usuario puede decidir el nombre para guardar el croquis closes #35

--- routes/index.js
+++ routes/index.js
@@ -245,11 +245,10 @@ module.exports = function(passport){
 		if (!req.files)
 			return res.status(400).send('No files were uploaded.');
 
-		// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-		let sampleFile = req.files.sketch;
+			var sketch = req.files.sketch;
 
 		// Use the mv() method to place the file somewhere on your server
-		sampleFile.mv('/home/cervi/Images/sketch.png', function (err) {
+		sketch.mv('/home/cervi/Images/'+req.files.sketch.name, function (err) {
 			if (err)
 				return res.status(500).send(err);
 		});
